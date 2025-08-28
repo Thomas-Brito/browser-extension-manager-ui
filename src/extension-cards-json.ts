01345b887e852e31cc54e63c0dc6ba1491fa85a9
thomasbrito <thomasbrito.tjb@gmail.com>
2025-08-28 14:40:13 -0300

    Espero que funcione

--- src/extension-cards-json.ts
+++ src/extension-cards-json.ts
@@ -1,5 +1,5 @@
 export async function getObjectsFromJson() {
-  const response = await fetch("public/data.json");
+  const response = await fetch("data.json");
   if(!response) {
     throw new Error("Failed to load json");
   }
